68a61e3181d8ea0b33c9204115d736eab363b5b0
Braeden Hintze <jhaezhyr12@gmail.com>
2026-02-03 15:11:03 -0700

    Add a ListenerForWhenIAmTargetedInATest
    It doesn't actually filter by which person is being targeted. That's bad.

--- swift/Sources/stormlight-duel/game/Listeners/AllTheListeners.swift
+++ swift/Sources/stormlight-duel/game/Listeners/AllTheListeners.swift
@@ -1,6 +1,7 @@
 public typealias AllTheListenersHolder = ListenerHolder & SelfListenerHolder
     & SelfListenerSelfHookHolder
     & SelfListenerSelfHookForTestHolder
+    & ListenerForWhenIAmTargetedInATestHolder
 
 public protocol NonLeafGenericListenerHolder {
     var childHolders: [Any] { get }
