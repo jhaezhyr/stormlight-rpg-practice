7283bbcfab2e840532173fb7194948f6f5b56a75
Braeden Hintze <jhaezhyr12@gmail.com>
2026-02-03 17:50:25 -0700

    fix: Add ListenerForWhenIAmTargetedInATestHolderLeaf to the LeafCondition protocol

--- swift/Sources/stormlight-duel/game/Condition/LeafCondition/LeafCondition.swift
+++ swift/Sources/stormlight-duel/game/Condition/LeafCondition/LeafCondition.swift
@@ -1,4 +1,16 @@
-internal protocol LeafCondition: Condition, ListenerHolderLeaf, SelfListenerHolderLeaf,
-    SelfListenerSelfHookHolderLeaf, SelfListenerSelfHookForTestHolderLeaf
+internal protocol LeafCondition: Condition,
+    AllTheListenersHolder,
+    ListenerHolderLeaf,
+    SelfListenerHolderLeaf,
+    SelfListenerSelfHookHolderLeaf,
+    SelfListenerSelfHookForTestHolderLeaf,
+    ListenerForWhenIAmTargetedInATestHolderLeaf
 {
+}
+
+/// If this needs any extra api, then the definition of LeafCondition is incomplete.
+struct DummyLeafCondition: LeafCondition {
+    var snapshot: any ConditionSnapshot
+    var id: Int
+
 }
