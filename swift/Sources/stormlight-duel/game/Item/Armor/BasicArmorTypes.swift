2f6c3eebb0a69109fa308b680b0bec350957f4b2
Braeden Hintze <jhaezhyr12@gmail.com>
2026-02-19 09:20:13 -0700

    Add all the basic armor and basic weapons into the game

--- swift/Sources/stormlight-duel/game/Item/Armor/BasicArmorTypes.swift
+++ swift/Sources/stormlight-duel/game/Item/Armor/BasicArmorTypes.swift
@@ -20,5 +20,36 @@ enum BasicArmorTypes {
             price: 60, weight: 10, armorType: .leather, deflect: 1,
             traits: [(trait: Presentable(), condition: .expert)], in: gameSession)
     }
-    // TODO the rest
+    static func chain(in gameSession: isolated GameSession) -> BasicArmor {
+        BasicArmor(
+            price: 80, weight: 25, armorType: .chain, deflect: 2,
+            traits: [
+                (trait: CumbersomeArmor(minStrength: 3), condition: .notExpert)
+                // TODO: Expert traits: Unique?
+            ], in: gameSession)
+    }
+    static func breastplate(in gameSession: isolated GameSession) -> BasicArmor {
+        BasicArmor(
+            price: 120, weight: 30, armorType: .breastplate, deflect: 1,
+            traits: [
+                (trait: CumbersomeArmor(minStrength: 3), condition: .always),
+                (trait: Presentable(), condition: .expert),
+            ], in: gameSession)
+    }
+    static func halfPlate(in gameSession: isolated GameSession) -> BasicArmor {
+        BasicArmor(
+            price: 400, weight: 40, armorType: .halfPlate, deflect: 3,
+            traits: [
+                (trait: CumbersomeArmor(minStrength: 4), condition: .notExpert),
+                (trait: CumbersomeArmor(minStrength: 3), condition: .expert),
+                // TODO: Expert traits: Unique?
+            ], in: gameSession)
+    }
+    static func fullPlate(in gameSession: isolated GameSession) -> BasicArmor {
+        BasicArmor(
+            price: 1600, weight: 55, armorType: .fullPlate, deflect: 4,
+            traits: [(trait: CumbersomeArmor(minStrength: 5), condition: .always)], in: gameSession)
+    }
+    // TODO Shardplate
+    // TODO Shardplate (Radiant)
 }
