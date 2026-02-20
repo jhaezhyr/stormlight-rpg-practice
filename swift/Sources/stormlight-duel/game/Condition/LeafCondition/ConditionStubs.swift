f4fc89cb325b84a814d4ce3ffd610e2e4962b015
Braeden Hintze <jhaezhyr12@gmail.com>
2026-02-20 15:55:18 -0700

    refactor: Imply gameSession

--- swift/Sources/stormlight-duel/game/Condition/LeafCondition/ConditionStubs.swift
+++ swift/Sources/stormlight-duel/game/Condition/LeafCondition/ConditionStubs.swift
@@ -2,13 +2,13 @@
 
 public struct Disoriented: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
 public struct Empowered: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
@@ -16,7 +16,8 @@ public struct Enhanced: Condition, ConditionSnapshot {
     public let id: Int
     public var stat: AttributeName
     public var amount: Int
-    public init(stat: AttributeName, amount: Int, in gameSession: isolated GameSession) {
+    public init(stat: AttributeName, amount: Int, in gameSession: isolated GameSession = #isolation)
+    {
         self.id = gameSession.nextId()
         self.stat = stat
         self.amount = amount
@@ -25,56 +26,56 @@ public struct Enhanced: Condition, ConditionSnapshot {
 public struct Exhausted: Condition, ConditionSnapshot {
     public let id: Int
     public var amount: Int
-    public init(amount: Int, in gameSession: isolated GameSession) {
+    public init(amount: Int, in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
         self.amount = amount
     }
 }
 public struct Focused: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
 public struct Immobilized: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
 public struct Prone: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
 public struct Restrained: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
 public struct Slowed: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
 public struct Stunned: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
 public struct Surprised: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
 public struct Unconscious: Condition, ConditionSnapshot {
     public let id: Int
-    public init(in gameSession: isolated GameSession) {
+    public init(in gameSession: isolated GameSession = #isolation) {
         self.id = gameSession.nextId()
     }
 }
