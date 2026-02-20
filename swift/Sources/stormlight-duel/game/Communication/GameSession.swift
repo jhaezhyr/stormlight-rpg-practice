d6b2ace036dc682d9bbea9744bce8c1d166bf937
Braeden Hintze <jhaezhyr12@gmail.com>
2026-02-20 15:48:51 -0700

    feat: Make Shallan an archer!

--- swift/Sources/stormlight-duel/game/Communication/GameSession.swift
+++ swift/Sources/stormlight-duel/game/Communication/GameSession.swift
@@ -40,23 +40,14 @@ public actor GameSession {
                 brain: try await brainForPlayer(player1Ref),
                 in: session,
             )
-            let player2 = PlayerRpgCharacter(
-                name: "Shallan",
-                expertises: [],
-                equipment: [Readyable(basicWeapons[.knife]!(session), isReady: true)],
-                attributes: .init { _ in 2 },
-                ranksInCoreSkills: .init { _ in 0 },
-                ranksInOtherSkills: [:],
-                health: .init(maxValue: 12),
-                focus: .init(maxValue: 4),
-                investiture: .init(maxValue: 0),
-                reach: 0,
-                features: [],
-                conditions: [],
+            let player2Ref = RpgCharacterRef(name: "Shallan Archer")
+            let player2 = PrefabCharacters.archer(
+                ref: player2Ref,
+                isPlayer: false,
                 brain: Level1CpuBrain(
-                    for: RpgCharacterRef(name: "Shallan")
+                    for: player2Ref
                 ),
-                isPlayer: false
+                in: session,
             )
             session.game.characters.upsert(.init(player1))
             session.game.characters.upsert(.init(player2))
